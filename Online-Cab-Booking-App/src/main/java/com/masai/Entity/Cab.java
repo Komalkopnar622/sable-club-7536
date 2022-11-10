fc11496d21701da3205fe57bd4c62ce6351eb483
devrajts <103634904+devrajts@users.noreply.github.com>
2022-11-10 10:30:55 +0530

    day2

--- Online-Cab-Booking-App/src/main/java/com/masai/Entity/Cab.java
+++ Online-Cab-Booking-App/src/main/java/com/masai/Entity/Cab.java
@@ -1,4 +1,4 @@
-package com.masai.model;
+package com.masai.Entity;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
--- Online-Cab-Booking-App/src/main/java/com/masai/Entity/Driver.java
+++ Online-Cab-Booking-App/src/main/java/com/masai/Entity/Driver.java
@@ -1,4 +1,4 @@
-package com.masai.model;
+package com.masai.Entity;
 
 import java.util.List;
 
