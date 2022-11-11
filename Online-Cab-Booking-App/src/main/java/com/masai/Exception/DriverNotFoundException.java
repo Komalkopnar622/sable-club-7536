e6f43a2c200959cc0feacba03b394036a9fc28b3
devrajts <103634904+devrajts@users.noreply.github.com>
2022-11-11 20:45:17 +0530

    day4

---
+++ Online-Cab-Booking-App/src/main/java/com/masai/Exception/DriverNotFoundException.java
@@ -0,0 +1,15 @@
+package com.masai.Exception;
+
+public class DriverNotFoundException extends Exception{
+	
+	public DriverNotFoundException() {
+		
+	}
+	
+    public DriverNotFoundException(String message) {
+    	
+    	super(message);
+		
+	}
+
+}
