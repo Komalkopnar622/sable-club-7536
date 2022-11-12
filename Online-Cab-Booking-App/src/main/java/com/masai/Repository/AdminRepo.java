14556193a61870126b1cd2d727b13d01e85bb0af
Komal Kopnar <104093194+Komalkopnar622@users.noreply.github.com>
2022-11-12 19:34:58 +0530

    Delete AdminRepo.java

--- Online-Cab-Booking-App/src/main/java/com/masai/Repository/AdminRepo.java
+++
@@ -1,8 +0,0 @@
-package com.masai.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.masai.Entity.*;
-
-public interface AdminRepo extends JpaRepository<Admin, Integer> {
-
-}
