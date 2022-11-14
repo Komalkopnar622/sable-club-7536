1076836a3b779c31ea7c50021e16a3795cc4cfaf
devrajts <103634904+devrajts@users.noreply.github.com>
2022-11-14 13:22:26 +0530

    update

--- Online-Cab-Booking-App/src/main/java/com/masai/Entity/Address.java
+++ Online-Cab-Booking-App/src/main/java/com/masai/Entity/Address.java
@@ -29,7 +29,36 @@ public class Address {
 	private String pincode;
 
 	public Integer getId() {
-		// TODO Auto-generated method stub
-		return null;
+		return id;
 	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+
+	
 }
